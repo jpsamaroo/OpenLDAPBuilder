8040041c1afa9388c6985941576087c3520dc654
Julian P Samaroo <jpsamaroo@gmail.com>
2018-08-20 16:52:39 -0500

    Added patches directory

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -10,21 +10,23 @@ sources = [
     "ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.46.tgz" =>
     "9a90dcb86b99ae790ccab93b7585a31fbcbeec8c94bf0f7ab0ca0a87ea0c4b2d",
 
+    "patches",
+
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 cd openldap-2.4.46/
-wget http://www.linuxfromscratch.org/patches/blfs/svn/openldap-2.4.46-consolidated-1.patch
-patch -Np1 -i openldap-2.4.46-consolidated-1.patch 
+#wget http://www.linuxfromscratch.org/patches/blfs/svn/openldap-2.4.46-consolidated-1.patch
+patch -Np1 -i ../openldap_patches/openldap-2.4.46-consolidated-1.patch 
 autoconf
 ./configure --prefix=$prefix --host=$target --with-yielding-select=yes --disable-static --enable-dynamic --disable-debug --disable-slapd
 apk update
 apk add groff binutils
 make depend
 make -j${nproc}
-make install
+make install || true # This is a hack
 
 """
 
